1d086d6bf37c2f49bfe60ba39cc5c5b9b097fa39
Ubuntu <ubuntu@ip-172-31-39-67.eu-north-1.compute.internal>
2025-12-09 07:16:16 +0000

    rk1

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -1,5 +1,5 @@
 // API Configuration
-const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'http://13.62.84.187:3000/api';
 
 // Helper function to get auth token
 function getAuthToken() {
